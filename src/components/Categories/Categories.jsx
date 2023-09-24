3873d742a2ef7bc728a5b5dbb674df1e9d1f5b4a
naymurabir <naymurrahman.abir@gmail.com>
2023-09-25 00:10:45 +0600

    Banner with a background image is created where used overlay of the image

---
+++ src/components/Categories/Categories.jsx
@@ -0,0 +1,16 @@
+import { useEffect, useState } from "react";
+
+
+const Categories = () => {
+
+
+
+
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default Categories;
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,4 +1,5 @@
 import Banner from "../Banner/Banner";
+import Categories from "../Categories/Categories";
 
 
 
@@ -6,6 +7,7 @@ const Home = () => {
     return (
         <div>
             <Banner></Banner>
+            <Categories></Categories>
         </div>
     );
 };
