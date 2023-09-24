b40b1a23e9316f9ea6b251d9ec9e3e05fd80a41a
naymurabir <naymurrahman.abir@gmail.com>
2023-09-24 22:45:06 +0600

    Responsive navbar is created and 3 titles of the navigation path is done.

---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,19 @@
+
+const Banner = () => {
+    return (
+
+        <div className="hero h-[400px]" style={{ backgroundImage: 'url(https://i.ibb.co/6nMKLGW/team.jpg)' }}>
+            <div className="hero-overlay bg-opacity-80 bg-white"></div>
+            <div className="hero-content text-center text-neutral-content">
+                <div className="max-w-md">
+                    <h1 className="mb-5 text-2xl font-bold text-black">I Grow By Helping People In Need</h1>
+
+                    <input className="px-2 py-1 bg-white border border-gray-300 rounded-md" type="text" placeholder="Search Here..." />
+                    <button className="text-white bg-red-500 font-bold, px-2 py-1 rounded-md">Search</button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,50 @@
+import { useState } from "react";
+import { NavLink } from "react-router-dom";
+import { HiOutlineMenuAlt1, HiMenuAlt4 } from 'react-icons/hi';
+import './Header.css'
+import logo from '../../../public/Logo.png'
+
+
+const Header = () => {
+
+
+    const [open, setOpen] = useState(false)
+
+    const handleDropDown = () => {
+        setOpen(!open)
+    }
+
+    return (
+
+        <nav className=" text-black py-2">
+
+            <div className="flex justify-between items-center">
+
+                <div>
+                    <img className="w-40" src={logo} alt="" />
+                </div>
+
+                <div className="relative" >
+                    <div className={`flex  flex-col md:flex-row absolute md:static duration-1000 px-2 py-3 text-black ${open ? 'top-5  -left-11 z-10' : ' -left-11 -top-40'}`}>
+
+                        <NavLink className="mr-6 text-lg font-semibold" to="/">Home</NavLink>
+
+                        <NavLink className="mr-6 text-lg font-semibold" to="/donation">Donation</NavLink>
+
+                        <NavLink className="mr-6 text-lg font-semibold" to="/statistics">Statistics</NavLink>
+
+                    </div>
+                </div>
+
+                <div onClick={handleDropDown} className="md:hidden text-xl">
+                    {
+                        open ? <HiOutlineMenuAlt1></HiOutlineMenuAlt1> : <HiMenuAlt4></HiMenuAlt4>
+                    }
+                </div>
+
+            </div>
+        </nav>
+    );
+};
+
+export default Header;
