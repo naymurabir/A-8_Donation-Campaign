0043bb2bb5ccd831c0d21500f204ec079fea60df
naymurabir <naymurrahman.abir@gmail.com>
2023-09-25 07:16:26 +0600

    See all button is added and all cards are displayed after clicking see all button

--- src/components/Donation/Donation.jsx
+++ src/components/Donation/Donation.jsx
@@ -6,9 +6,9 @@ import MakeDonation from "../makeDonation/makeDonation";
 
 const Donation = () => {
 
-
     const donations = useLoaderData()
     const [donated, setDonated] = useState([])
+    const [dataLength, setDataLength] = useState(true)
 
     useEffect(() => {
         const storedDonations = getStoredDonations()
@@ -20,20 +20,24 @@ const Donation = () => {
                     makeDonated.push(donation)
                 }
                 setDonated(makeDonated)
-
             }
-
-
         }
     }, [donations])
 
     return (
 
-        <div className="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-2 gap-5">
-            {
-                donated.map((donate, idx) => <MakeDonation key={idx} donate={donate}></MakeDonation>)
-            }
+        <div>
+            <div className="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-2 gap-5 mt-10">
+                {
+                    dataLength ? donated.slice(0, 4).map((donate, idx) => <MakeDonation key={idx} donate={donate}></MakeDonation>) : donated.map((donate, idx) => <MakeDonation key={idx} donate={donate}></MakeDonation>)
+                }
+            </div>
+            <div className={`flex justify-center mt-5 ${dataLength ? 'block' : 'hidden'}`}>
+                <button onClick={() => setDataLength(!dataLength)}
+                    className='bg-green-700 px-3 py-2 text-white font-semibold rounded-md'>See All</button>
+            </div>
         </div>
+
     );
 };
 
